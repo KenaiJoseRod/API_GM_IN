b4457ae2c670dad4680c34601af6e981ee0c168a
KenaiJoseRod <jr805036@gmail.com>
2024-09-13 10:29:22 -0500

    web api

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const mysql = require('mysql2');
 const myconn = require('express-myconnection');
 const cors = require('cors'); // Importa el paquete cors
 const routes = require('./routes');
-
+require('dotenv').config();
 const app = express();
 
 app.set('port', process.env.PORT || 9000);
 
 const dbOptions = {
-    host: 'mysql-27664253-jr805036-084d.k.aivencloud.com',
-    port: 11289,
-    user: 'avnadmin',
-    password: 'AVNS_j9QXeuWqs8y9EbANPoK',
-    database: 'defaultdb'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
 
 // Usa el middleware cors antes de tus rutas
